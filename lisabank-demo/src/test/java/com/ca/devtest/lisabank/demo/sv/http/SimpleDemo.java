9b5b59bda8619f8046d7beb0288666ee4458ff2a
gaspa03 <gaspa03@ca.com>
2017-09-27 15:25:16 +0300

    initial commit

---
+++ lisabank-demo/src/test/java/com/ca/devtest/lisabank/demo/sv/http/SimpleDemo.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package com.ca.devtest.lisabank.demo.sv.http;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.ca.devtest.lisabank.demo.LisaBankClientApplication;
+import com.ca.devtest.lisabank.demo.business.BankService;
+import com.ca.devtest.lisabank.wsdl.Account;
+import com.ca.devtest.lisabank.wsdl.User;
+import com.ca.devtest.sv.devtools.annotation.DevTestVirtualServer;
+import com.ca.devtest.sv.devtools.annotation.DevTestVirtualService;
+import com.ca.devtest.sv.devtools.annotation.Parameter;
+import com.ca.devtest.sv.devtools.annotation.Protocol;
+import com.ca.devtest.sv.devtools.annotation.ProtocolType;
+import com.ca.devtest.sv.devtools.junit.VirtualServicesRule;
+
+/**
+ * @author pascal.gasp@ca.com
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = LisaBankClientApplication.class)
+
+public class SimpleDemo {
+	static final Log logger = LogFactory.getLog(SimpleDemo.class);
+	@Autowired
+	private BankService bankServices;
+
+
+	@Test
+	public void getListUser() {
+		User[] users = bankServices.getListUser();
+		assertNotNull(users);
+		assertEquals(9, users.length);
+	}
+
+	
+	private void printUsers(User[] users) {
+		for (User user : users) {
+			logger.info(user.getFname() + " " + user.getLname() + " " + user.getLogin());
+		}
+
+	}
+}
