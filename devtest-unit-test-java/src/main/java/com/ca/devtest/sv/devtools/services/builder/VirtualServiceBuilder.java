f93693f0d791c148aefc47cf88ab3ec62286dee6
gaspa03 <gaspa03@ca.com>
2017-10-12 09:56:53 +0200

    fixed Deployed Name issue - Value set from builder to VirtualService object

--- devtest-unit-test-java/src/main/java/com/ca/devtest/sv/devtools/services/builder/VirtualServiceBuilder.java
+++ devtest-unit-test-java/src/main/java/com/ca/devtest/sv/devtools/services/builder/VirtualServiceBuilder.java
@@ -22,56 +22,54 @@ import com.ca.devtest.sv.devtools.utils.VelocityRender;
  * @author gaspa03
  *
  */
-public  abstract class VirtualServiceBuilder   implements ParamatrizedBuilder{
-	
+public abstract class VirtualServiceBuilder implements ParamatrizedBuilder {
 
+	private VirtualServiceEnvironment vse;
+	private String serviceName;
+	private TransportProtocolDefinition transportProtocol;
+	private final String DEFAULT_SERVICE_PROPERTIES_TPL = "<?xml version=\"1.0\" ?><recording><name${virtualService.deployedName}</name><binary>false</binary><group>$virtualService.group</group></recording>";
+	private final Map<String, String> parameters = new HashMap<String, String>();
+	private VirtualServiceType type = VirtualServiceType.RRPAIRS;
 
-	private  VirtualServiceEnvironment vse;
-	private  String serviceName;
-	private  TransportProtocolDefinition transportProtocol;
-	private final String DEFAULT_SERVICE_PROPERTIES_TPL="<?xml version=\"1.0\" ?><recording><name${virtualService.deployedName}</name><binary>false</binary><group>$virtualService.group</group></recording>";
-	private final Map<String, String> parameters= new HashMap<String, String>();
-	private VirtualServiceType type=VirtualServiceType.RRPAIRS;
-	
-	public VirtualServiceBuilder(String name, VirtualServiceEnvironment vse){
-		super(); 
-		this.serviceName=name;
-		this.vse=vse;
-		transportProtocol=new TransportProtocolBuilderImpl(TransportProtocolType.HTTP.getType()).addParameter("listenPort", "8080").addParameter("basePath", "/").build();
+	public VirtualServiceBuilder(String name, VirtualServiceEnvironment vse) {
+		super();
+		this.serviceName = name;
+		this.vse = vse;
+		transportProtocol = new TransportProtocolBuilderImpl(TransportProtocolType.HTTP.getType())
+				.addParameter("listenPort", "8080").addParameter("basePath", "/").build();
 	}
-	
+
 	/**
 	 * @param transportProtocol
 	 * @return
 	 */
-	public VirtualServiceBuilder overHttp(int port , String basePath) {
-		
-		this.transportProtocol=new TransportProtocolBuilderImpl(TransportProtocolType.HTTP.getType()).addParameter("listenPort", String.valueOf(port)).addParameter("basePath", basePath).build();
+	public VirtualServiceBuilder overHttp(int port, String basePath) {
+
+		this.transportProtocol = new TransportProtocolBuilderImpl(TransportProtocolType.HTTP.getType())
+				.addParameter("listenPort", String.valueOf(port)).addParameter("basePath", basePath).build();
 		return this;
 	}
-	
+
 	/**
 	 * @param transportProtocol
 	 * @return
 	 */
 	public VirtualServiceBuilder over(TransportProtocolDefinition transportProtocol) {
-		
-		this.transportProtocol=transportProtocol;
+
+		this.transportProtocol = transportProtocol;
 		return this;
 	}
-	
-	
+
 	/**
 	 * @return
-	 * @throws IOException 
+	 * @throws IOException
 	 */
-	public final VirtualService build() throws IOException{
-		VirtualService  virtualService = new VirtualService(getServiceName(),getType(),getVse()); 
-	
+	public final VirtualService build() throws IOException {
+		VirtualService virtualService = new VirtualService(getServiceName(), getType(), getVse());
+		virtualService.setDeployedName(getDeployedName());
 		virtualService.setPackedVirtualService(packVirtualService());
 		return virtualService;
 	}
-
 
 	/**
 	 * @return the type
@@ -81,7 +79,8 @@ public  abstract class VirtualServiceBuilder   implements ParamatrizedBuilder{
 	}
 
 	/**
-	 * @param type the type to set
+	 * @param type
+	 *            the type to set
 	 */
 	public final void setType(VirtualServiceType type) {
 		this.type = type;
@@ -93,6 +92,7 @@ public  abstract class VirtualServiceBuilder   implements ParamatrizedBuilder{
 	protected final VirtualServiceEnvironment getVse() {
 		return vse;
 	}
+
 	/**
 	 * @return the transportProtocol
 	 */
@@ -100,80 +100,78 @@ public  abstract class VirtualServiceBuilder   implements ParamatrizedBuilder{
 		return transportProtocol;
 	}
 
-	
 	/**
 	 * @return the serviceName
 	 */
 	public final String getServiceName() {
 		return serviceName;
 	}
-	
+
 	/**
 	 * @return the group
 	 */
 	public final String getGroup() {
 		return getVse().getGroup();
 	}
+
 	/**
 	 * @return the serviceName
 	 */
 	public final String getDeployedName() {
-		return getGroup()+"."+getServiceName();
+		return getGroup() + "." + getServiceName();
 	}
-	
+
 	/**
 	 * @return
 	 */
 	protected String generateServicePropertiesContent() {
-		
-		Map<String, Object> config= new HashMap<String, Object>();
+
+		Map<String, Object> config = new HashMap<String, Object>();
 		config.put("virtualService", this);
 		return VelocityRender.render(DEFAULT_SERVICE_PROPERTIES_TPL, config);
 	}
-	
 
 	/**
 	 * @return
 	 * @throws IOException
 	 */
-	protected abstract File packVirtualService()throws IOException ;
+	protected abstract File packVirtualService() throws IOException;
 
 	/**
 	 * @return
 	 */
 	protected String generateVrsContent() {
-		
-		return null!=transportProtocol?transportProtocol.toVrsContent():"";
+
+		return null != transportProtocol ? transportProtocol.toVrsContent() : "";
 	}
 
 	/**
 	 * @param dataProtocol
 	 * @return
 	 */
-	public  VirtualServiceBuilder addRequestDataProtocol(DataProtocolDefinition dataProtocol) {
-		
-		 transportProtocol.getRequestSide().add(dataProtocol);
-		 return this;
+	public VirtualServiceBuilder addRequestDataProtocol(DataProtocolDefinition dataProtocol) {
+
+		transportProtocol.getRequestSide().add(dataProtocol);
+		return this;
 	}
-	
+
 	/**
 	 * @param dataProtocol
 	 * @return
 	 */
 	public VirtualServiceBuilder addRespondDataProtocol(DataProtocolDefinition dataProtocol) {
-		
-		 transportProtocol.getResponseSide().add(dataProtocol);
-		 return this;
+
+		transportProtocol.getResponseSide().add(dataProtocol);
+		return this;
 	}
 
 	protected Map<String, String> getParameters() {
 		return parameters;
 	}
-	
 
 	@Override
 	public void addKeyValue(String key, String value) {
 		parameters.put(key, value);
-		
+
 	}
 }
